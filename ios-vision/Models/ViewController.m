47d4f9f9b0085a4cfa6d6145e0b3fb2082c8139c
Ji Zhou <jz_1106@163.com>
2020-11-17 22:35:41 +0800

    still image

--- ios-vision/Models/ViewController.m
+++ ios-vision/Models/ViewController.m
@@ -9,13 +9,44 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UIButton *stillImageBtn;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self configBaseUI];
+}
+
+#pragma mark - BaseSet
+- (void)configBaseUI{
+    [self.view addSubview:self.stillImageBtn];
+    [self.stillImageBtn makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).offset(100);
+        make.left.equalTo(self.view).offset(10);
+        make.right.equalTo(self.view).offset(-10);
+        make.height.equalTo(40);
+    }];
+}
+
+#pragma mark - Lazy
+
+- (UIButton *)stillImageBtn{
+    if (!_stillImageBtn) {
+        UIButton *button = [[UIButton alloc]init];
+        [button setBackgroundImage:nil forState:UIControlStateNormal];
+        [button setTitle:@"Still Image Analysis" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(stillImageBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+        button.backgroundColor=UIColor.brownColor;
+        _stillImageBtn = button;
+    }
+    return _stillImageBtn;
+}
+
+-(void)stillImageBtnAction:(UIButton*)btn{
+    
 }
 
 
