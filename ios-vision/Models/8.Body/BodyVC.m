b54a0ec5e3db32544a42c2d0bf04c42a5a2a1313
David Ji <297933227@qq.com>
2020-11-23 18:06:07 +0800

    in company

--- ios-vision/Models/8.Body/BodyVC.m
+++ ios-vision/Models/8.Body/BodyVC.m
@@ -8,10 +8,13 @@
 #import "BodyVC.h"
 #import <Vision/Vision.h>
 
-@interface BodyVC ()
+@interface BodyVC (){
+    int _imgIndex;
+}
 
 @property (nonatomic,strong) UIImageView *coverImageView;
 @property (strong,nonatomic) UIButton *bodyBtn;
+@property (strong,nonatomic) UIButton *handBtn;
 
 @end
 
@@ -20,7 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self configBaseUI];
-//    _imgIndex=0;
+    _imgIndex=0;
 }
 
 #pragma mark - BaseSet
@@ -37,6 +40,14 @@
     [self.view addSubview:self.bodyBtn];
     [self.bodyBtn makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.coverImageView.bottom).offset(10);
+        make.left.equalTo(self.view).offset(10);
+        make.right.equalTo(self.view).offset(-10);
+        make.height.equalTo(40);
+    }];
+    
+    [self.view addSubview:self.handBtn];
+    [self.handBtn makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.bodyBtn.bottom).offset(10);
         make.left.equalTo(self.view).offset(10);
         make.right.equalTo(self.view).offset(-10);
         make.height.equalTo(40);
@@ -68,11 +79,46 @@
     return _bodyBtn;
 }
 
+- (UIButton *)handBtn{
+    if (!_handBtn) {
+        UIButton *button = [[UIButton alloc]init];
+        [button setBackgroundImage:nil forState:UIControlStateNormal];
+        [button setTitle:@"Detecting Human Hand Poses in Images" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(handBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+        button.backgroundColor=UIColor.brownColor;
+        _handBtn = button;
+    }
+    return _handBtn;
+}
+
 #pragma mark - Action
 
 -(void)bodyBtnAction:(UIButton*)btn{
+    for(CALayer *subLayer in self.coverImageView.layer.sublayers){
+        [subLayer removeFromSuperlayer];
+    }
+    //
+    _imgIndex++;
+    if (_imgIndex>3){
+        _imgIndex=0;
+    }
     UIImage *image;
     image=[UIImage imageNamed:@"人像"];
+    switch (_imgIndex) {
+        case 0:
+            image=[UIImage imageNamed:@"人像"];
+            break;
+        case 1:
+            image=[UIImage imageNamed:@"人像2"];
+            break;
+        case 2:
+            image=[UIImage imageNamed:@"人像3"];
+            break;
+        default:
+            image=[UIImage imageNamed:@"人像4"];
+            break;
+    }
+    
     self.coverImageView.image=image;
     CGImageRef cgImage=image.CGImage;
     //
@@ -82,18 +128,147 @@
     if (@available(iOS 14.0, *)) {
         request=[[VNDetectHumanBodyPoseRequest alloc]initWithCompletionHandler:^(VNRequest * _Nonnull request, NSError * _Nullable error) {
             for(VNObservation *observation in request.results){
-                if ([observation isKindOfClass:VNRecognizedPointsObservation.class]){
+                if ([observation isKindOfClass:VNHumanBodyPoseObservation.class]){
+                    VNHumanBodyPoseObservation *pointsObservation=(VNHumanBodyPoseObservation *)observation;
+                    for (VNRecognizedPointKey pointKey in pointsObservation.availableKeys){
+                        NSLog(@"PointKey:%@",pointKey);
+                    }
+                    for (VNRecognizedPointGroupKey pointGroupKey in pointsObservation.availableGroupKeys){
+                        NSLog(@"PointGroupKey:%@",pointGroupKey);
+                    }
+                    for (VNHumanBodyPoseObservationJointName jointName in pointsObservation.availableJointNames){
+                        NSLog(@"JointName:%@",jointName);
+                    }
+                    for (VNHumanBodyPoseObservationJointsGroupName jointGroupName in pointsObservation.availableJointsGroupNames){
+                        NSLog(@"JointName:%@",jointGroupName);
+                    }
+                    NSDictionary<VNRecognizedPointKey,VNRecognizedPoint *> * pointDict=[pointsObservation recognizedPointsForGroupKey:VNRecognizedPointGroupKeyAll error:nil];
+                    NSArray *keys = [pointDict allKeys];
+                    NSMutableArray *pointsArg=[NSMutableArray array];
+                    for (int i = 0; i < keys.count; i++){
+                        VNRecognizedPointKey key = [keys objectAtIndex: i];
+                        VNRecognizedPoint * point = [pointDict objectForKey: key];
+                        NSLog (@"RecognizedPoint  Key:%@ , Point: { key:%@ , confidence:%f , point:[%f,%f] ,location:[%f,%f] }", key, point.identifier , point.confidence ,point.x,point.y, point.location.x, point.location.y);
+                        [pointsArg addObject:[NSValue valueWithCGPoint:point.location]];
+                    }
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [self drawAtLayer:self.coverImageView.layer cgImage:cgImage pointsArg:pointsArg];
+                    });
                 }
             }
         }];
     }
-    
     //3. Perform Requests
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSError *error;
         if ([imageRequestHandler performRequests:@[request] error:&error]){
         }
     });
+}
+
+-(void)handBtnAction:(UIButton*)btn{
+    for(CALayer *subLayer in self.coverImageView.layer.sublayers){
+        [subLayer removeFromSuperlayer];
+    }
+    //
+    _imgIndex++;
+    if (_imgIndex>3){
+        _imgIndex=0;
+    }
+    UIImage *image;
+    image=[UIImage imageNamed:@"手"];
+    switch (_imgIndex) {
+        case 0:
+            image=[UIImage imageNamed:@"手"];
+            break;
+        case 1:
+            image=[UIImage imageNamed:@"手"];
+            break;
+        case 2:
+            image=[UIImage imageNamed:@"手"];
+            break;
+        default:
+            image=[UIImage imageNamed:@"手4"];
+            break;
+    }
+    
+    self.coverImageView.image=image;
+    CGImageRef cgImage=image.CGImage;
+    //
+    VNImageRequestHandler *imageRequestHandler=[[VNImageRequestHandler alloc]initWithCGImage:cgImage orientation:kCGImagePropertyOrientationUp options:@{}];
+    //
+    VNRequest *request;
+    if (@available(iOS 14.0, *)) {
+        request=[[VNDetectHumanHandPoseRequest alloc]initWithCompletionHandler:^(VNRequest * _Nonnull request, NSError * _Nullable error) {
+            for(VNObservation *observation in request.results){
+                if ([observation isKindOfClass:VNHumanHandPoseObservation.class]){
+                    VNHumanHandPoseObservation *pointsObservation=(VNHumanHandPoseObservation *)observation;
+                    for (VNRecognizedPointKey pointKey in pointsObservation.availableKeys){
+                        NSLog(@"PointKey:%@",pointKey);
+                    }
+                    for (VNRecognizedPointGroupKey pointGroupKey in pointsObservation.availableGroupKeys){
+                        NSLog(@"PointGroupKey:%@",pointGroupKey);
+                    }
+                    for (VNHumanBodyPoseObservationJointName jointName in pointsObservation.availableJointNames){
+                        NSLog(@"JointName:%@",jointName);
+                    }
+                    for (VNHumanBodyPoseObservationJointsGroupName jointGroupName in pointsObservation.availableJointsGroupNames){
+                        NSLog(@"JointName:%@",jointGroupName);
+                    }
+                    NSDictionary<VNRecognizedPointKey,VNRecognizedPoint *> * pointDict=[pointsObservation recognizedPointsForGroupKey:VNRecognizedPointGroupKeyAll error:nil];
+                    NSArray *keys = [pointDict allKeys];
+                    NSMutableArray *pointsArg=[NSMutableArray array];
+                    for (int i = 0; i < keys.count; i++){
+                        VNRecognizedPointKey key = [keys objectAtIndex: i];
+                        VNRecognizedPoint * point = [pointDict objectForKey: key];
+                        NSLog (@"RecognizedPoint  Key:%@ , Point: { key:%@ , confidence:%f , point:[%f,%f] ,location:[%f,%f] }", key, point.identifier , point.confidence ,point.x,point.y, point.location.x, point.location.y);
+                        [pointsArg addObject:[NSValue valueWithCGPoint:point.location]];
+                    }
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [self drawAtLayer:self.coverImageView.layer cgImage:cgImage pointsArg:pointsArg];
+                    });
+                }
+            }
+        }];
+    }
+    //3. Perform Requests
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSError *error;
+        if ([imageRequestHandler performRequests:@[request] error:&error]){
+        }
+    });
+}
+
+#pragma mark - Function
+
+-(void)drawAtLayer:(CALayer*)layer cgImage:(CGImageRef)cgImage pointsArg:(NSArray*)pointsArg{
+    CGFloat imgWidth = CGImageGetWidth(cgImage);
+    CGFloat imgHeight = CGImageGetHeight(cgImage);
+    CGRect layerBound = layer.bounds;
+    CGFloat ratio=1.0f;
+    if ((imgWidth/imgHeight)<(layerBound.size.width/layerBound.size.height)){
+        ratio=layerBound.size.height/imgHeight;
+    }else{
+        ratio=layerBound.size.width/imgWidth;
+    }
+    CGRect shapeLayerBound=CGRectMake(0, 0, imgWidth*ratio, imgHeight*ratio);
+    CAShapeLayer *shapeLayer=[CAShapeLayer layer];
+    shapeLayer.bounds=shapeLayerBound;
+    shapeLayer.position=CGPointMake(layerBound.size.width/2, layerBound.size.height/2);
+    UIBezierPath *path=[UIBezierPath bezierPath];
+    for(NSValue * pointValue in pointsArg){
+        CGPoint normalizedPoint=[pointValue CGPointValue];
+        CGPoint imagePoint=VNImagePointForNormalizedPoint(normalizedPoint,shapeLayerBound.size.width,
+                                                          shapeLayerBound.size.height);
+        CGPoint layerPoint=CGPointMake(imagePoint.x, shapeLayerBound.size.height-imagePoint.y);
+        [path moveToPoint:layerPoint];
+        [path addArcWithCenter:layerPoint radius:2 startAngle:0 endAngle:M_PI * 2 clockwise:YES];
+    }
+    shapeLayer.path=path.CGPath;
+    shapeLayer.strokeColor=UIColor.redColor.CGColor;
+    shapeLayer.fillColor=UIColor.clearColor.CGColor;
+    shapeLayer.lineWidth=4;
+    [layer addSublayer:shapeLayer];
 }
 
 @end
